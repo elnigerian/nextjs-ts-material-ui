54bbc2bee8c6bdabcc9d72d25ec9b1e6bcc08e90
elnigerian <aaodutayo@gmail.com>
2019-08-23 19:47:02 -0400

    first commit of trial implementation

---
+++ components/Copyright.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Link from "./Link";
+
+export const Copyright:React.FunctionComponent = () => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'. Built with '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Material-UI.
+            </Link>
+        </Typography>
+    );
+}
